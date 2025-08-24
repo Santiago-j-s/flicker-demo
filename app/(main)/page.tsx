8093cf4e4b93c8647a16dc5508d06f0f1df4d76a
Santiago Santana <santana.santiago@gmail.com>
2025-08-24 18:11:30 -0300

    feat: update Home component layout to include header and display fetched database value

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -7,11 +7,18 @@ export default async function Home() {
   const value = await getValueFromDatabase();
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-4xl font-bold tracking-tight text-center text-gray-900 dark:text-white mb-4">
-        Testing Dialog
-      </h1>
-      <Modal value={value} />
+    <div className="font-sans grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <header>
+        <h1 className="text-4xl font-bold tracking-tight text-center text-gray-900 dark:text-white mb-4">
+          Testing Dialog
+        </h1>
+        <p className="text-sm text-gray-500 font-bold">
+          Value from database: {value}
+        </p>
+      </header>
+      <div className="flex flex-col gap-2">
+        <Modal value={value} />
+      </div>
     </div>
   );
 }
